3abedb10bfde9482b2568e73e66ad9c378eb7870
Brian <hibrian827@gmail.com>
2024-01-08 00:48:01 +0900

    solved shell_basic totally

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,7 +1,4 @@
 from pwn import remote
 
-def messageToHex(m):
-  return hex(bytes_to_long(m.encode("utf-8")))[2:].encode("utf-8")
-
-port = 11526
-rem = remote("host1.dreamhack.games", port)
+port = 17904
+rem = remote("host3.dreamhack.games", port)
