ff3950abdaf9df085156d0898d8ef28efb05faec
Brian <hibrian827@gmail.com>
2024-03-19 22:41:50 +0900

    solving CBC

--- WHS/Exercise-CBC/problem.py
+++ WHS/Exercise-CBC/problem.py
@@ -11,25 +11,25 @@ decrypt = lambda ct: unpad(AES.new(key, AES.MODE_CBC, key).decrypt(ct), BLOCK_SI
 
 print("Welcome to dream's AES server")
 while True:
-    print("[1] Encrypt")
-    print("[2] Decrypt")
-    print("[3] Get Flag")
+  print("[1] Encrypt")
+  print("[2] Decrypt")
+  print("[3] Get Flag")
 
-    choice = input()
+  choice = input()
 
-    if choice == "1":
-        print("Input plaintext (hex): ", end="")
-        pt = bytes.fromhex(input())
-        print(encrypt(pt).hex())
+  if choice == "1":
+    print("Input plaintext (hex): ", end="")
+    pt = bytes.fromhex(input())
+    print(encrypt(pt).hex())
 
-    elif choice == "2":
-        print("Input ciphertext (hex): ", end="")
-        ct = bytes.fromhex(input())
-        print(decrypt(ct).hex())
+  elif choice == "2":
+    print("Input ciphertext (hex): ", end="")
+    ct = bytes.fromhex(input())
+    print(decrypt(ct).hex())
 
-    elif choice == "3":
-        print(f"flag = {encrypt(flag).hex()}")
-        exit()
+  elif choice == "3":
+    print(f"flag = {encrypt(flag).hex()}")
+    exit()
 
-    else:
-        print("Nope")
+  else:
+    print("Nope")
--- WHS/main.py
+++ WHS/main.py
@@ -1,19 +1,30 @@
 from pwn import remote
+import binascii
+
+def toHex(msg) :
+  res = []
+  for c in msg :
+    temp = str(int(c, 16))
+    res.append(temp)
+  return ''.join(res)
 
 def sendCmd(rem,num) :
   for i in range(3) :
     print(rem.recvline())
     rem.sendline(str(num).encode())    
 
-port = 13709
+port = 21590
 rem = remote("srv1.kitriwhs.kr", port)
 
 print(rem.recvline())
 
-sendCmd(rem, 3)
-encoded_flag = rem.recvline().decode('utf-8')
-flag_val = encoded_flag[encoded_flag.index('=') + 2 : ]
+# sendCmd(rem, 3)
+# encoded_flag = rem.recvline().decode('utf-8')
+# flag_val = encoded_flag[encoded_flag.index('=') + 2 : ]
 
-sendCmd(rem, 1)
-rem.sendline(flag_val.encode())
-print(rem.recvline())
+# sendCmd(rem, 1)
+# print(rem.recvall())
+# rem.sendline(toHex(b"a"))
+# print(rem.recvline())
+
+print(toHex("a"))
