0e57776c539cc082397bf177207f591b3773a94b
Brian <hibrian827@gmail.com>
2024-03-20 15:39:39 +0900

    still working on CBC

--- WHS/main.py
+++ WHS/main.py
@@ -1,19 +1,16 @@
 from pwn import remote
+from Crypto.Util.number import bytes_to_long
 import binascii
 
-def toHex(msg) :
-  res = []
-  for c in msg :
-    temp = str(int(c, 16))
-    res.append(temp)
-  return ''.join(res)
+def messageToHex(m):
+  return hex(bytes_to_long(m.encode("utf-8")))[2:].encode("utf-8")
 
 def sendCmd(rem,num) :
   for i in range(3) :
     print(rem.recvline())
     rem.sendline(str(num).encode())    
 
-port = 21590
+port = 24401
 rem = remote("srv1.kitriwhs.kr", port)
 
 print(rem.recvline())
@@ -21,10 +18,9 @@ print(rem.recvline())
 # sendCmd(rem, 3)
 # encoded_flag = rem.recvline().decode('utf-8')
 # flag_val = encoded_flag[encoded_flag.index('=') + 2 : ]
+# print(flag_val)
 
-# sendCmd(rem, 1)
-# print(rem.recvall())
-# rem.sendline(toHex(b"a"))
-# print(rem.recvline())
-
-print(toHex("a"))
+sendCmd(rem, 1)
+print(rem.recvuntil(b": "))
+rem.sendline(messageToHex('hello'))
+print(rem.recvall())
