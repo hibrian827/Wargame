c8a25500b3ae466f4d58008f75d8e1b59962bfe6
Brian <hibrian827@gmail.com>
2024-03-21 01:10:39 +0900

    still solving

--- WHS/main.py
+++ WHS/main.py
@@ -3,24 +3,29 @@ from Crypto.Util.number import bytes_to_long
 import binascii
 
 def messageToHex(m):
-  return hex(bytes_to_long(m.encode("utf-8")))[2:].encode("utf-8")
+  return binascii.hexlify(m.encode())
 
-def sendCmd(rem,num) :
-  for i in range(3) :
-    print(rem.recvline())
-    rem.sendline(str(num).encode())    
+def sendCmd(rem, num) :
+  print(rem.recvuntil(b"Flag\n"))
+  rem.sendline(str(num).encode())
+  if num == 1 or num == 2:
+    rem.recvuntil(b": ")
 
-port = 24401
+port = 11675
 rem = remote("srv1.kitriwhs.kr", port)
 
 print(rem.recvline())
 
 # sendCmd(rem, 3)
 # encoded_flag = rem.recvline().decode('utf-8')
-# flag_val = encoded_flag[encoded_flag.index('=') + 2 : ]
+# flag_val = encoded_flag[encoded_flag.index('=') + 2 : -1]
 # print(flag_val)
 
 sendCmd(rem, 1)
-print(rem.recvuntil(b": "))
-rem.sendline(messageToHex('hello'))
-print(rem.recvall())
+rem.sendline(messageToHex("hello"))
+test = rem.recvline().decode()
+print(test)
+
+sendCmd(rem, 2)
+rem.sendline(test.encode())
+print(bytes.fromhex(rem.recvline().decode()).decode("utf-8"))
