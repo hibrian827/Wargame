93f575fb1be52b6106a002bcef7cdc37ea795015
Brian <hibrian827@gmail.com>
2024-04-13 16:18:09 +0900

    still solving

--- WHS/main.py
+++ WHS/main.py
@@ -3,6 +3,9 @@ from pwn import *
 port = 5000
 rem = remote("52.79.50.147", port)
 
+elf = ELF("./WHS/system_hacking_hw/problem/libc.so.6")
+print(list(elf.search(b"main")))
+
 payload = b"hi"
 rem.sendlineafter(b"post?", payload)
 rem.recvuntil(b"post:\n")
