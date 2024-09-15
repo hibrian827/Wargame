00a6aa21dc58915007fd7719d061693c360f2679
Brian Lee <hibrian827@gmail.com>
2024-09-15 22:49:51 +0900

    added send_sig

---
+++ Dreamhack/Pwnable/send_sig/answer/disassemble.c
@@ -0,0 +1,18 @@
+ssize_t sub_4010B6()
+{
+  char buf[8]; // [rsp+8h] [rbp-8h] BYREF
+
+  write(1, "Signal:", 7uLL);
+  return read(0, buf, 0x400uLL);
+}
+
+void __noreturn start()
+{
+  setvbuf(stdout, 0LL, 2, 0LL);
+  setvbuf(stdin, 0LL, 1, 0LL);
+  write(1, "++++++++++++++++++Welcome to dreamhack++++++++++++++++++\n", 0x39uLL);
+  write(1, "+ You can send a signal to dreamhack server.           +\n", 0x39uLL);
+  write(1, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", 0x39uLL);
+  sub_4010B6();
+  exit(0);
+}
