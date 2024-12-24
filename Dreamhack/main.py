21549ccc225b2955f1afa577df1eb223aeac6a28
Brian Lee <hibrian827@gmail.com>
2024-12-25 02:01:45 +0900

    solving pizza

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,5 +1,12 @@
 from pwn import *
 
+rem = process("./Pwnable/pizza/problem/pizza")
+gdb.attach(rem)
+
 elf = ELF("./Pwnable/pizza/problem/pizza")
 
-print(elf.symbols['main'])
+rem.sendlineafter(b"name > ", b"test")
+
+rem.interactive()
+
+# print(elf.symbols)
