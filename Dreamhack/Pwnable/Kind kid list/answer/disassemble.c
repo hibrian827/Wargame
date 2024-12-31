a2c64700b7d6711572d44855a7ef674dd6b5475f
Brian Lee <hibrian827@gmail.com>
2025-01-01 02:14:16 +0900

    solving Kind kid list

--- Dreamhack/Pwnable/Kind kid list/answer/disassemble.c
+++ Dreamhack/Pwnable/Kind kid list/answer/disassemble.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <signal.h>
+
+void busted()
+{
+  puts("\n\nYou BUSTED\n\n");
+  exit(0);
+}
+
+unsigned int init()
+{
+  setvbuf(stdin, 0LL, 2, 0LL);
+  setvbuf(stdout, 0LL, 2, 0LL);
+  setvbuf(stderr, 0LL, 2, 0LL);
+  signal(14, (__sighandler_t)busted);
+  return alarm(0x3Cu);
+}
 
 int flag()
 {
@@ -10,10 +26,18 @@ int flag()
   return puts(s);
 }
 
+int menu()
+{
+  puts("\n1. Open santa's notebook and look at it");
+  puts("2. Add name to kind list");
+  puts("3. Go back to wyv3rn");
+  return printf(">> ");
+}
+
+
 int main(int argc, const char **argv, const char **envp)
 {
   char *v3; // rsi
-  char *v4; // rdi
   long v5; // rdx
   long v6; // rcx
   long v7; // r8
@@ -24,11 +48,8 @@ int main(int argc, const char **argv, const char **envp)
   long v12; // r8
   long v13; // r9
   char s2[8]; // [rsp+0h] [rbp-E0h] BYREF
-  long v15; // [rsp+8h] [rbp-D8h]
   char v16[8]; // [rsp+10h] [rbp-D0h] BYREF
-  long v17; // [rsp+18h] [rbp-C8h]
   char dest[8]; // [rsp+20h] [rbp-C0h] BYREF
-  long v19; // [rsp+28h] [rbp-B8h]
   char v20[132]; // [rsp+30h] [rbp-B0h] BYREF
   char src[8]; // [rsp+B4h] [rbp-2Ch] BYREF
   int v22; // [rsp+BCh] [rbp-24h] BYREF
@@ -45,27 +66,22 @@ int main(int argc, const char **argv, const char **envp)
   v25 = 0;
   v22 = 0;
   v26 = 0;
-  *(long *)src = 0x6E7233767977LL;
-  memset(v20, 0, 0x80uLL);
-  *(long *)dest = 0LL;
-  v19 = 0LL;
-  *(long *)v16 = 0LL;
-  v17 = 0LL;
-  *(long *)s2 = 0LL;
-  v15 = 0LL;
-  ptr = malloc(8uLL);
+  *(long *)src = 0x6E7233767977;
+  memset(v20, 0, 0x80);
+  *(long *)dest = 0;
+  *(long *)v16 = 0;
+  *(long *)s2 = 0;
+  ptr = malloc(8);
   stream = fopen("/dev/urandom", "r");
-  fread(ptr, 7uLL, 1uLL, stream);
+  fread(ptr, 7, 1, stream);
   v3 = src;
-  v4 = dest;
   strcpy(dest, src);
 
   while ( 1 )
   {
-    menu(v4, v3, v5, v6, v7, v8);
+    menu();
     fflush(stdout);
     v3 = (char *)&v22;
-    v4 = "%d";
     scanf("%d", &v22);
     if ( v22 == 3 ) break;
     if ( v22 <= 3 )
@@ -76,8 +92,7 @@ int main(int argc, const char **argv, const char **envp)
         for ( i = 0; i <= 5; ++i ) puts(&v20[16 * i]);
         puts("\n-Naughty kid list-");
         for ( i = 0; i <= 7; ++i ) putchar(dest[i]);
-        v4 = (char *)&byte_2072;
-        puts(&byte_2072);
+        puts("");
       }
       else if ( v22 == 2 )
       {
@@ -85,29 +100,22 @@ int main(int argc, const char **argv, const char **envp)
         fflush(stdout);
         scanf("%8s", s2);
         v3 = s2;
-        if ( !strncmp((const char *)ptr, s2, 7uLL) )
+        if ( !strncmp((const char *)ptr, s2, 7) )
         {
           printf("Name : ");
           fflush(stdout);
           v3 = v16;
           scanf("%8s", v16);
-          if ( v26 > 7 )
-          {
-            v4 = "Kind list full";
-            puts("Kind list full");
-          }
+          if ( v26 > 7 ) puts("Kind list full");
           else
           {
             v3 = v16;
-            v4 = &v20[16 * v26];
-            strcpy(v4, v16);
             ++v26;
           }
         }
         else
         {
           printf(s2);
-          v4 = " is Wrong password!";
           puts(" is Wrong password!");
         }
       }
@@ -115,7 +123,6 @@ int main(int argc, const char **argv, const char **envp)
   }
   
   v25 = 0;
-  v9 = "wyv3rn";
   if ( !strcmp(v20, "wyv3rn") )
   {
     for ( i = 0; i <= 5; ++i )
