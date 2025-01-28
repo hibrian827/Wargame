efec2e47e402025f8643e095a137f214b30e5348
Brian <hibrian827@gmail.com>
2025-01-28 17:45:07 +0900

    minor changes

--- Dreamhack/Pwnable/send_sig/answer/disassemble.c
+++ Dreamhack/Pwnable/send_sig/answer/disassemble.c
@@ -3,9 +3,9 @@ const char *sub_401090()
   return "/bin/sh";
 }
 
-__int64 sub_4010A6()
+long sub_4010A6()
 {
-  __int64 savedregs; // [rsp+0h] [rbp+0h]
+  long savedregs; // [rsp+0h] [rbp+0h]
 
   return savedregs;
 }
@@ -14,17 +14,17 @@ ssize_t sub_4010B6()
 {
   char buf[8]; // [rsp+8h] [rbp-8h] BYREF
 
-  write(1, "Signal:", 7uLL);
-  return read(0, buf, 0x400uLL);
+  write(1, "Signal:", 7);
+  return read(0, buf, 0x400);
 }
 
-void __noreturn start()
+void start()
 {
-  setvbuf(stdout, 0LL, 2, 0LL);
-  setvbuf(stdin, 0LL, 1, 0LL);
-  write(1, "++++++++++++++++++Welcome to dreamhack++++++++++++++++++\n", 0x39uLL);
-  write(1, "+ You can send a signal to dreamhack server.           +\n", 0x39uLL);
-  write(1, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", 0x39uLL);
+  setvbuf(stdout, 0, 2, 0);
+  setvbuf(stdin, 0, 1, 0);
+  write(1, "++++++++++++++++++Welcome to dreamhack++++++++++++++++++\n", 0x39);
+  write(1, "+ You can send a signal to dreamhack server.           +\n", 0x39);
+  write(1, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", 0x39);
   sub_4010B6();
   exit(0);
 }
