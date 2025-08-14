fadb48c6e8bf05ab2d7a9315d13e51c81995f51b
Brian Lee <hibrian827@gmail.com>
2025-08-15 00:36:50 +0900

    solving iofile_vtable_check

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -4,6 +4,16 @@ context.terminal = ['tmux', 'new-window']
 
 success = lambda name, addr: log.success(f"{name}: {hex(addr)}")
 
-ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/ld-linux-x86-64.so.2"
-env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/toxic_malloc/problem/deploy/libc.so.6"}
-rem = process([ld, "./Pwnable/toxic_malloc/problem/deploy/chall"], env=env)
+env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/iofile_vtable_check/problem/libc.so.6"}
+rem = process("./Pwnable/iofile_vtable_check/problem/iofile_vtable_check", env=env)
+
+# elf = ELF("./Pwnable/iofile_vtable_check/problem/iofile_vtable_check")
+# lib = ELF("./Pwnable/iofile_vtable_check/problem/libc.so.6")
+
+# rem.recvuntil(b"stdout: ")
+# stdout_addr = int(rem.recvn(14).decode(), 16)
+# lib_base = stdout_addr - lib.symbols['_IO_2_1_stdout_']
+
+# success("lib base", lib_base)
+
+rem.interactive()
