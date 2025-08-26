19bb077ba85ef5e1820839789151d801074ef251
Brian <hibrian827@gmail.com>
2025-08-26 15:22:46 +0900

    changed library file

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -1,8 +1,13 @@
 from pwn import *
 
-# context.terminal = ['tmux', 'new-window']
+context.terminal = ['tmux', 'new-window']
 
 success = lambda name, addr: log.success(f"{name}: {hex(addr)}")
 
-env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Holymoly/problem/libc-2.31.so"}
-rem = process("./Pwnable/Holymoly/problem/holymoly")
+env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/Heap_Basic_1/problem/deploy/libc-2.31.so"}
+ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/Heap_Basic_1/problem/deploy/ld-linux-x86-64.so.2"
+rem = process([ld, "./Pwnable/Heap_Basic_1/problem/deploy/prob"], env=env)
+
+gdb.attach(rem)
+
+rem.interactive()
