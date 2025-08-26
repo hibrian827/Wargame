f6f5ab268b5ce88fc27ab1b009c6a229d6af9430
Brian <hibrian827@gmail.com>
2025-08-26 15:30:08 +0900

    testing

--- Dreamhack/main.py
+++ Dreamhack/main.py
@@ -5,7 +5,7 @@ context.terminal = ['tmux', 'new-window']
 success = lambda name, addr: log.success(f"{name}: {hex(addr)}")
 
 env = {"LD_PRELOAD": "/home/hibrian827/Wargame/Dreamhack/Pwnable/Heap_Basic_1/problem/deploy/libc-2.31.so"}
-ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/Heap_Basic_1/problem/deploy/ld-linux-x86-64.so.2"
+ld = "/home/hibrian827/Wargame/Dreamhack/Pwnable/Heap_Basic_1/problem/deploy/ld-2.31.so"
 rem = process([ld, "./Pwnable/Heap_Basic_1/problem/deploy/prob"], env=env)
 
 gdb.attach(rem)
